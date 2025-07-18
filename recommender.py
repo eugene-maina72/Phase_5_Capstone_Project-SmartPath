bc1f747ce8555ec282f31924638acf142b609bf3
eugene-maina72 <eugene.maina@student.moringaschool.com>
2025-07-18 16:20:23 +0300

    final cluster model complete

--- recommender.py
+++ recommender.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
+from sklearn.decomposition import TruncatedSVD
 
 job_profiles_and_abilities = pd.read_csv('data/job_profiles_and_abilities.csv')
+job_profiles_with_skills = pd.read_csv('data/job_profiles_with_skills.csv')
 
 
 def recommend_jobs(user_profile, job_profiles_and_abilities=job_profiles_and_abilities, top_n=5):
@@ -89,3 +92,58 @@ def recommend_jobs(user_profile, job_profiles_and_abilities=job_profiles_and_abi
     # Returning the recommended jobs DataFrame
     
     return recommended_jobs
+
+
+def kmeans_svd_recommender(user_profile: dict, job_profile_with_skills=job_profiles_with_skills, n_clusters = 3, svd_components = 20, top_n: int = 5):
+
+    """
+    Recommends top N jobs based on user's RIASEC scores and education level and skills required.
+    Takes in a Dataframe, reduces its dimensionality using Truncated SVD to retain 20 of the most important components.
+    Clusters the data using K-Means Clustering then calculates cosine similarity of a user profile and the reduced Dataframe RIASEC + Education level
+    but does not use skills for cosine similarity.
+
+    Args:
+        user_profile (dict): Dictionary with keys 'R', 'I', 'A', 'S', 'E', 'C', 'education_level', and 'skills'.
+        job_profiles_and_abilities (pd.DataFrame): Job dataset containing RIASEC, education, and skill columns.
+        n_clusters (int) : The number of clusters to be instatiated in the KMeans.Optimized at 3(default)
+        svd_components (int): The number of features to be retained after in the truncated SVD algorithm
+        top_n (int, optional): Number of job recommendations to return. Defaults to 5.
+
+    Returns:
+        pd.DataFrame: DataFrame with top N recommended jobs sorted by similarity score.
+
+    """
+    # Extracting the RIASEC, Normalized Education score and the skill columns for machine learning
+    features = ['R', 'I', 'A', 'S', 'E', 'C', 'Normalized Education Score'] + job_profile_with_skills.columns[27:].tolist()
+    X = job_profile_with_skills[features].fillna(0).values
+    
+    # Reducing the number of features to improve generalization
+    svd = TruncatedSVD(n_components=svd_components, random_state=42)
+    X_reduced = svd.fit_transform(X)
+
+    #instatiating and fitting the model
+    model = KMeans(n_clusters=n_clusters, random_state=42)
+    labels = model.fit_predict(X_reduced)
+    job_profile_with_skills['Cluster'] = labels #Creating a new column for the cluster assignments
+
+    user_vector_raw = np.array([
+        user_profile['R']/7, user_profile['I']/7, user_profile['A']/7,
+        user_profile['S']/7, user_profile['E']/7, user_profile['C']/7,
+        user_profile['education_level']/12
+    ] + [0] * (X.shape[1] - 7)).reshape(1, -1)
+    user_vector_reduced = svd.transform(user_vector_raw)    # Creating a new user profile similar to the TruncatedSVD array
+
+    # Predicting the User's Cluster assignment and Calculating cosine similarity to determine best job match
+    user_cluster = model.predict(user_vector_reduced)[0]
+    svd_cluster_jobs = job_profile_with_skills[job_profile_with_skills['Cluster'] == user_cluster].copy()
+    sim_scores = cosine_similarity(user_vector_reduced, svd.transform(svd_cluster_jobs[features].fillna(0).values)).flatten()
+    svd_cluster_jobs['Similarity Score'] = sim_scores
+    
+    # Sort values based on cosine similarity and displaying top 5 matches
+    svd_top_jobs =svd_cluster_jobs.sort_values(by='Similarity Score', ascending=False).head(top_n)
+    return svd_top_jobs[['Title', 'Description', 
+                                         'Education Category Label',
+                                         'Education Level', 
+                                          'Preparation Level',
+                                            'Similarity Score',
+                                            'Normalized Education Score']].style.background_gradient(cmap='YlGn')
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SmartPathClustering',
+    version='1.0.0',
+    description='Career recommendation system using Machine learning Algorithim',
+    author='Eugene Maina, Beryl Okelo, Beth Nyambura, Allan Ofula, Rachael Nyawira ',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.23',
+        'pandas>=1.5',
+        'matplotlib>=3.6',
+        'scikit-learn>=1.2',
+        'hdbscan>=0.8.29'
+    ],
+    python_requires='>=3.8',
+)
